08e51e0fb60f9e5a25cfd38ebf156145c6ac269a
gautamsuranipromact <gautamsurani@promactinfo.com>
2022-03-16 12:18:03 +0530

    1

---
+++ generate-ios.sh
@@ -0,0 +1,7 @@
+mkdir -p Payload
+mv /Users/runner/work/flutter_cicd/flutter_cicd/build/ios/iphoneos/Runner.app Payload
+zip -r -y Payload.zip Payload/Runner.app
+mv Payload.zip Payload.ipa
+
+# the following are options, remove Payload folder
+rm -Rf Payload
